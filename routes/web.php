9b286298d1fd460b0fb88d55c58eb5ba2c20623e
ALI TAYYAB <01-134232-037@student.bahria.edu.pk>
2025-04-18 22:15:43 +0500

    update routes and Blade templates to use named routes for navigation and form actions

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\SignupController;
 use App\Http\Controllers\LoginController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
 
 Route::get('/',[HomeController::class,'index'])->name("home");
 
@@ -11,3 +12,7 @@ Route::get('/signup',[SignupController::class, 'create'])->name('signup');
 
 Route::get('/login',[LoginController::class, 'create'])->name('login');
 
+Route::get('/car/search',[CarController::class, 'search'])->name('car.search');
+Route::resource('car', CarController::class)->only([
+    'index', 'create', 'store', 'show', 'edit', 'update', 'destroy'
+]);
